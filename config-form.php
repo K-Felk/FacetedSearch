957b1621b747d20f88fe5ca42a4d76c2fa0f90bc
Kyle Felker <felkerk@gvsu.edu>
2017-06-22 14:29:40 -0400

    git add .

---
+++ config-form.php
@@ -0,0 +1,8 @@
+
+<div class="three columns alpha">
+    <label for="test_text"><?php echo __('Test Text option Input');?></label>
+</div>
+
+<div class="five columns omega">
+<input type='text' name='test_text' value='<?php echo get_option("test_text"); ?>' />
+</div>
